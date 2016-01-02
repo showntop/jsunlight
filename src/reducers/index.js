38a0117bd64396dde6967898ed454f2837e2beb6
showntop <rongtaoxiao@gmail.com>
2016-01-02 14:51:45 +0800

    init

--- src/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers }    from 'redux';
-import { routeReducer }       from 'redux-simple-router';
-import users                  from './users';
-
-export default combineReducers({
-  users
-});
